28d9cb2339280131e67d5736542e411525547986
appworld-hu <burai.sandor@gmail.com>
2023-07-31 22:53:00 +0200

    index.ejs file rendered

--- index.js
+++ index.js
@@ -3,12 +3,14 @@ const session = require("express-session");
 const { body, validationResult, matchedData } = require("express-validator");
 const flash = require("express-flash");
 const app = express();
+const path = require('path')
 
 const mongoose = require("mongoose");
 
 const ejs = require("ejs");
 app.set("view engine", ejs);
-
+app.set("views", path.join(__dirname, "/views"));
+ 
 app.use(express.urlencoded({ extended: true }));
 app.use(
   session({
@@ -25,7 +27,7 @@ async function main() {
   await mongoose.connect("mongodb://127.0.0.1:27017/mongoose_todo");
 
   app.get("/", (req, res) => {
-    res.status(200).send("The app is running with the imported libraries");
+    res.render('index.ejs');
   });
 
   app.listen(3000, () => {
